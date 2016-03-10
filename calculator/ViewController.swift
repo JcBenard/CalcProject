2be6e8eb7b8ff0d27abe18e52206435ad719cc7a
student <student@students-mac.student.georgianc.on.ca>
2016-03-10 13:35:51 -0500

    Hooked up the buttons and labels to the view controller

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -1,21 +1,72 @@
 //
 //  ViewController.swift
-//  calculator
+//  calculator app
 //
-//  Created by student on 2016-03-10.
+//  Created by Jean-Claude Benard on 2016-03-10.
 //  Copyright © 2016 student. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    
+    
+    @IBOutlet var displayLabel: UILabel!
+    
+    @IBAction func use0(sender: AnyObject){
+        displayLabel.text = displayLabel.text! + "0"
+    }
+    
+    @IBAction func use1(sender: AnyObject){
+        displayLabel.text = "1" + displayLabel.text!
         
     }
-
-
+    @IBAction func use2(sender: AnyObject){
+        
+    }
+    @IBAction func use3(sender: AnyObject){
+        
+    }
+    @IBAction func use4(sender: AnyObject){
+        
+    }
+    @IBAction func use5(sender: AnyObject){
+        
+    }
+    @IBAction func use6(sender: AnyObject){
+        
+    }
+    @IBAction func use7(sender: AnyObject){
+        
+    }
+    @IBAction func use8(sender: AnyObject){
+        
+    }
+    @IBAction func use9(sender: AnyObject){
+        
+    }
+    @IBAction func useDecimal(sender: AnyObject){
+        
+    }
+    @IBAction func useEqual(sender: AnyObject){
+        
+    }
+    @IBAction func useDivide(sender: AnyObject){
+        
+    }
+    @IBAction func useMultiply(sender: AnyObject){
+        
+    }
+    @IBAction func useSubtract(sender: AnyObject){
+        
+    }
+    @IBAction func useAdd(sender: AnyObject){
+        
+    }
+    @IBAction func useClear(sender: AnyObject){
+        
+    }
 
 }
 
